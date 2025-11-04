d7e43234dd8c2faee20a284022ac4355cba8dd62
2004Veena <veenalucky2004@gmail.com>
2025-11-04 11:31:17 +0530

    Add files via upload

---
+++ control flow statements.py
@@ -0,0 +1,13 @@
+# verify even or odd number ?
+'''num=int(input("enter a number :"))
+if num%2==0:
+    print("given number is even")
+else:
+    print("given number is odd")'''
+
+
+
+if 0:  # 1 for true ,,,,0 for false
+    print("good morning")
+else:
+    print("good night")
